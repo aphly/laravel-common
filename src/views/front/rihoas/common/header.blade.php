69199547f6b0bb4e738abc6ae85b8a7bd3d01827
Aphly <121099327@qq.com>
2022-11-17 16:43:21 +0800

    update

--- src/views/front/rihoas/common/header.blade.php
+++ src/views/front/rihoas/common/header.blade.php
@@ -1,7 +1,11 @@
 @include('laravel-common-front::common.header_common')
 <link rel="stylesheet" href="{{ URL::asset('static/common/css/iconfont.css') }}">
 <link rel="stylesheet" href="{{ URL::asset('static/common/css/common.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('static/common/css/font.css') }}">
 
+<style>
+    body{font-family: 'Montserrat';}
+</style>
 <header>
     <div>
 
