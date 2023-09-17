b97255b3254f2ab041356432f32f80b6d9ff6911
aphly <121099327@qq.com>
2023-09-17 19:49:33 +0800

    update

--- src/views/mail/footer.blade.php
+++ src/views/mail/footer.blade.php
@@ -1,10 +1,20 @@
+
     <div style="border-top: 1px solid #C6C6C6;margin: 20px 0;"></div>
     <div style="font-size: 12px;">
+        <ul style="list-style: none;display: flex;padding: 0 0px;font-size: 12px;">
+            <li ><a style="" href="{{url('/information/1')}}">About Us</a></li>
+            <li style="margin: 0 10px;color: #999;">|</li>
+            <li ><a style="" href="{{url('/account_ext/order')}}">My Order</a></li>
+            <li style="margin: 0 10px;color: #999;">|</li>
+            <li><a style="" href="{{url('/account_ext/service')}}">My Service</a></li>
+        </ul>
         <div style="margin-top: 10px;">
-            © {{date('Y')}} <a href="{{url('')}}" style="color: #333;">{{config('common.hostname')}}</a>. All Rights Reserved.
+            If you have any questions, please <a href="{{url('/contact_us')}}" style="color: #333;">contact us</a> or <a
+                href="mailto:{{config('common.email')}}" style="color: #333;">{{config('common.email')}}</a> through our customer support page.
         </div>
+
         <div style="margin-top: 10px;">
-            If you have any questions, please <a href="{{url('/contact_us')}}" style="color: #333;">contact us</a> through our customer support page.
+            © {{date('Y')}} <a href="{{url('')}}" style="color: #333;">{{config('common.name')}}</a> All Rights Reserved.
         </div>
     </div>
 </div>
