e47544c0d2e0f29de7a367b464878e47ff606e9b
Aphly <121099327@qq.com>
2023-09-26 10:13:04 +0800

    update

--- src/config/common.php
+++ src/config/common.php
@@ -4,14 +4,19 @@ return [
     'hostname'=>'adultsex.vip',
     'email'=>'adultsexvip@gmail.com',
     'perPage'=>'10',
+
     //注册类型
     'id_type'=>'email',  //email || mobile
+
     //邮件激活
     'email_verify'=>false,
+
     //发送邮件类型 0同步 1队列
     'email_type'=>1,
+
     //发送邮件队列通道 1vip 0普通
     'email_queue_priority'=>0,
+
     //快捷注册
     'oauth'=>[
         'type'=>'id', //id || email
@@ -20,8 +25,10 @@ return [
             'google',
         ]
     ],
+
     'template'=>'rihoas',
     //'template'=>'blog',
+
     'menu'=>[
         'about_us'=>'/information/1',
         'terms_of_service'=>'/information/2',
@@ -32,7 +39,8 @@ return [
         'payment'=>'/information/7',
         'faq'=>'/information/8',
     ],
-    'statistics_appid'=>'2023080134801599',
+
+    'statistics_appid'=>'2023092600946526',
     'email_appid'=>'2023080188980024',
     'email_secret'=>'nw30ZFKpOGjm3KwIoWcTSgbiRs6RXR3k',
 
